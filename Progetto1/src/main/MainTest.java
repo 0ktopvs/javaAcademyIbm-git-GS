09c0760efae3ffd5918bcd58d2f6904264f0c47f
GennaroSigillo <gennarosigillo13@gmail.com>
2022-06-15 14:38:27 +0200

    primo commit eclipse

---
+++ Progetto1/src/main/MainTest.java
@@ -0,0 +1,12 @@
+package main;
+
+import classi.Persona;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Persona p=new Persona ("Gennaro", "Sigillo");
+		System.out.println(p.toString());
+	}
+
+}
